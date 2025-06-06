334f80f177dee3d0c83764bb0433d9d3c008c700
cyrus0w <cyrus0w@github.com>
2025-06-06 19:22:54 +0800

    add

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 #echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default
-echo "src-git qbittorrent https://github.com/KryptonLee/qBittorrent-openwrt-package.git;v4.4.5-openwrt-21.02" >> ./feeds.conf.default
+#echo "src-git qbittorrent https://github.com/KryptonLee/qBittorrent-openwrt-package.git;v4.4.5-openwrt-21.02" >> ./feeds.conf.default
 #echo "src-git qbittorrent https://github.com/sbwml/luci-app-qbittorrent.git;4.4.5" >> ./feeds.conf.default
 #echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
 #sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
